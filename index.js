b7598b97d5762ea87d7604701c38f976a57e1b7d
nyakiri_margaret <margaret.karanja@student.moringaschool.com>
2022-12-02 10:22:55 +0300

    Completed assig

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+const cities = ["Nairobi","kisumu","Nakuru,"]
+
+function writeCards(cities , party){
+console.log(`writeCards${cities} Thank you,Nairobi for the wonderfull time!`);
+for (let i =0; i< cities.length; i++){
+
+}
+writeCards(["Nairobi","kisumu","Nakuru"], "cities")
+
+return cities;
+}
+writeCards(cities);
+
+
+
+
+
+function countDown(){
+    const countDown = 0;
+
+}
+while (countDown < 11){
+    console.log(countdown++);
+}
